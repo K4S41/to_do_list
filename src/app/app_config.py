81b233319d24b1f58757267e924db91931cff799
ezzop6 <ezzop6@gmail.com>
2024-02-11 10:08:23 +0100

    Added CORS settings for allowed origins

--- src/app/app_config.py
+++ src/app/app_config.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+ALLOWED_ORIGINS = [origin.strip() for origin in os.getenv("ALLOWED_ORIGINS", "").split(",")]
+
 
 @dataclass
 class AppConfig:
@@ -13,3 +15,11 @@ class AppConfig:
     PORT: int = int(os.getenv("APP_PORT", 5000))
     DEBUG: bool = bool(os.getenv("APP_DEBUG", False))
     HOST: str = os.getenv("APP_HOST", "0.0.0.0")
+
+
+@dataclass
+class CorsConfig:
+    """
+    CORS configuration
+    """
+    ORIGINS = ALLOWED_ORIGINS
