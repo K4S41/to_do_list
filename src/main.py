2d04d0344eed776c6b712364135cfbba6aeafdf8
Petr <146182785+K4S41@users.noreply.github.com>
2024-02-20 10:55:34 +0100

    :boom: project restart

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from app.app_config import AppConfig
-
-if __name__ == "__main__":
-    app.run(
-        port=AppConfig.PORT,
-        host=AppConfig.HOST,
-        debug=AppConfig.DEBUG
-    )  # type: ignore
